466fa4d4242b4d825c2b480b06fea103b4c1f2e1
Tarik EN NAKDI <t.ennakdi@gmail.com>
2016-10-25 15:45:13 +0200

    Deep Learning and Data Science
    All the basics

---
+++ examples/linear_regression_class/generate_1d.py
@@ -0,0 +1,15 @@
+# generates 1-dimensional data for linear regression analysis
+#
+# notes for this course can be founda at:
+# https://www.udemy.com/data-science-linear-regression-in-python
+
+
+import numpy as np
+
+N = 100
+with open('data_1d.csv', 'w') as f:
+    X = np.random.uniform(low=0, high=100, size=N)
+    Y = 2*X + 1 + np.random.normal(scale=5, size=N)
+    for i in xrange(N):
+        f.write("%s,%s\n" % (X[i], Y[i]))
+
---
+++ examples/nlp_class2/ner_rnn.py
@@ -0,0 +1,15 @@
+# Course URL: https://udemy.com/natural-language-processing-with-deep-learning-in-python
+from ner_baseline import get_data
+from pos_rnn import RNN
+
+def main():
+    Xtrain, Ytrain, Xtest, Ytest, word2idx, tag2idx = get_data(split_sequences=True)
+    V = len(word2idx)
+    rnn = RNN(10, [10], V)
+    rnn.fit(Xtrain, Ytrain, epochs=70)
+    print "train f1 score:", rnn.f1_score(Xtrain, Ytrain)
+    print "test f1 score:", rnn.f1_score(Xtest, Ytest)
+    
+
+if __name__ == '__main__':
+    main()
